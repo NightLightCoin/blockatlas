15c3933a5c309ea9dec6aeb30a1bec0b094ac279
Mykola <kolya182@gmail.com>
2020-03-30 20:27:05 -0700

    REmove v2 collections route

--- api/routes.go
+++ api/routes.go
@@ -43,15 +43,11 @@ func SetupPlatformAPI(root gin.IRouter) {
 	}
 
 	for _, collectionAPI := range platform.Platforms {
-		routerV2 := getRouter(v2, collectionAPI.Coin().Handle)
 		routerV3 := getRouter(v3, collectionAPI.Coin().Handle)
 		routerV4 := getRouter(v4, collectionAPI.Coin().Handle)
 
 		makeCollectionsRoute(routerV3, collectionAPI)
 		makeCollectionRoute(routerV3, collectionAPI)
-
-		oldMakeCollectionRoute(routerV2, collectionAPI)
-		oldMakeCollectionsRoute(routerV2, collectionAPI)
 
 		makeCollectionRouteV4(routerV4, collectionAPI)
 	}
@@ -66,7 +62,6 @@ func SetupPlatformAPI(root gin.IRouter) {
 	MakeLookupRoute(ns)
 	MakeLookupBatchRoute(batchNs)
 
-	oldMakeCategoriesBatchRoute(v2)
 	makeCategoriesBatchRoute(v3)
 	makeCategoriesBatchRouteV4(v4)
 	makeStakingDelegationsBatchRoute(v2)
